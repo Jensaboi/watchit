631ecf5ede0997b17cdc35d1761dc72b0faff32c
Jens <jens.lundin1005@gmail.com>
2026-04-04 11:10:47 +0200

    Wip mobile nav

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router";
 import { X } from "lucide-react";
 import Logo from "../assets/Logo";
+import { useState } from "react";
 
 const navItems = [
   { name: "Movies", path: "/movie" },
@@ -9,28 +10,45 @@ const navItems = [
 ];
 
 export default function MainLayout() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <div className="h-screen w-full flex flex-col">
       <header>
-        <nav className="translate-0 p-4 absolute right-0 bg-zinc-800 w-full h-screen flex flex-col">
-          <div className="py-4 flex justify-between align-center">
+        <nav
+          data-isOpen={navOpen}
+          className="translate-0 absolute right-0 bg-zinc-900 w-full h-screen flex flex-col data-isOpen[true]:translate-x-full"
+        >
+          <div className="p-4 flex justify-between align-center">
             <Logo />
 
-            <button className="border-indigo-400 border-2 rounded-sm cursor-pointer hover:shadow-indigo-400 p-1 hover:shadow-xs ">
+            <button
+              onClick={() => setNavOpen(!navOpen)}
+              className="bg-indigo-500/20 hover:bg-indigo-500/30 rounded-sm cursor-pointer hover:shadow-indigo-400/50 p-2 hover:shadow-xs "
+            >
               <X size={20} />
             </button>
           </div>
-          <ul className="flex flex-col">
+
+          <ul className="flex flex-col flex-1 p-2">
             {navItems.map(item => (
               <li>
-                <Link to={item.path}>{item.name}</Link>
+                <Link
+                  className="block py-3 px-2 hover:bg-zinc-800 rounded-md"
+                  to={item.path}
+                >
+                  {item.name}
+                </Link>
               </li>
             ))}
           </ul>
 
-          <div className="flex flex-col">
-            <Link to={"/signin"}>Sign in</Link>
-            <Link to={"/signup"}>Sign up</Link>
+          <div className="flex p-4 flex-col gap-2">
+            <Link className="primary-btn" to={"/signin"}>
+              Sign in
+            </Link>
+            <Link className="secondary-btn" to={"/signup"}>
+              Sign up
+            </Link>
           </div>
         </nav>
       </header>
