5dbbd3ca27269e2ce1307b97580f27a25c9f3f64
Jens <jens.lundin1005@gmail.com>
2026-04-11 11:13:31 +0200

    Media details page

--- frontend/src/pages/MediaDetailsPage.tsx
+++ frontend/src/pages/MediaDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { queryClient } from "../App";
-import { useLoaderData, useParams } from "react-router";
+import { useLoaderData, useParams, useRouteLoaderData } from "react-router";
 import { getMediaDetails } from "../service/apiService";
 import Error from "../components/Error";
 import MovieDetails from "../features/movie/components/MovieDetails";
@@ -27,6 +27,8 @@ export async function loader({ params }) {
 export default function MediaDetailsPage() {
   const { query } = useLoaderData();
 
+  const { imgConfig } = useRouteLoaderData("app");
+
   const { media, id } = useParams();
 
   const { data, isLoading, isError, error } = useQuery(query);
@@ -35,9 +37,41 @@ export default function MediaDetailsPage() {
 
   if (isError) return <Error error={error} />;
 
-  if (media === "movie" && id) return <MovieDetails movie={data} />;
+  if (media !== "movie" && media !== "tv") return <NotFound />;
 
-  if (media === "tv" && id) return <TvDetails show={data} />;
+  if (!id) return <NotFound />;
 
-  return <NotFound />;
+  const mediaName =
+    media === "movie" ? data?.title : media === "tv" ? data?.name : "";
+
+  const posterBaseUrl =
+    imgConfig.secure_base_url +
+    imgConfig.poster_sizes[imgConfig.poster_sizes.length - 1];
+
+  const backdropBaseUrl =
+    imgConfig.secure_base_url +
+    imgConfig.backdrop_sizes[imgConfig.backdrop_sizes.length - 1];
+
+  return (
+    <div className="w-full h-full">
+      <div className="relative w-full h-full max-h-150">
+        <div>{/*backdrop*/}</div>
+        <img
+          className="absolute z-1 inset-0 w-full h-full object-cover object-center"
+          src={backdropBaseUrl + data.backdrop_path}
+          alt={`${mediaName} backdrop image`}
+        />
+        <div className="w-full h-full flex p-8 gap-8">
+          <img
+            className="z-10 rounded-md shadow-lg"
+            src={posterBaseUrl + data?.poster_path}
+            alt={`${mediaName} poster`}
+          />
+          <section className="z-10 w-full h-full py-4">
+            <h1 className="text-2xl font-bold">{mediaName}</h1>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
